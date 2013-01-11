ba692be05e3f006af61b8783b4494b7e69fe78e6
Your Name <sean@process255.com>
2013-01-11 10:13:36 -0700

    added snippets for instance and class methods

---
+++ cm.m
@@ -0,0 +1,13 @@
+// Class Method
+// creating a new clas method
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
++ (<#return_type#>)<#method_name#>;
+
++ (<#return_type#>)<#method_name#>
+{
+    <#code#>
+}
---
+++ im.m
@@ -0,0 +1,11 @@
+// Instance Method
+// creating a new instance method
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+- (<#return_type#>)<#method_name#>
+{
+    <#code#>
+}
